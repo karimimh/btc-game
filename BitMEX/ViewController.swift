f979abd9ed1cc4d24f8a21992081b56c86b38856
Behnam Karimi <bmhkarimi@gmail.com>
2019-09-10 19:28:21 +0430

    Initial Commit

---
+++ BitMEX/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BitMEX
+//
+//  Created by Behnam Karimi on 6/19/1398 AP.
+//  Copyright © 1398 AP Behnam Karimi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
